eaa136c9ce1a30776481bf95d8d451411f6f1a74
Ishmita910 <31636876+Ishmita910@users.noreply.github.com>
2018-10-25 21:09:05 -0400

    jquery

--- news_transfer/static/js/news_transfer.js
+++ news_transfer/static/js/news_transfer.js
@@ -4,6 +4,16 @@
 // Copyright 2017 University of New Hapmshire by Dr. Alex Prusevich.
 // alex.proussevitch@unh.edu
 //*****************************************************************************
+$(document).ready(function() {
+  setInterval(function() {
+    cache_clear()
+  }, 3000);
+});
+
+function cache_clear() {
+  window.location.reload(true);
+  // window.location.reload(); use this if you do not remove cache
+}
 
 var $TABLE = $('#table');
 var $BTN = $('#export-btn');
